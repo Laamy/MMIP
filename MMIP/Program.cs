333b00cdb735a1c16e303f1b96f52fa1ca7b5a2b
yeemi routh <Shade4Keal@gmail.com>
2023-01-19 18:52:54 +1100

    Add project files.

---
+++ MMIP/Program.cs
@@ -0,0 +1,23 @@
+﻿using MMIP.SDK;
+using System;
+using System.IO;
+
+namespace MMIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mcManifest =  @"C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.5101.0_x64__8wekyb3d8bbwe\AppxManifest.xml";
+
+            MMIP_API.TakeOwnership(mcManifest);
+            MMIP_API.GrantFullControl(mcManifest);
+
+            MMIP_API.SystemCopy(mcManifest, new FileInfo("AppxManifest (1).xml").FullName);
+
+            File.Delete(mcManifest); // delete original
+
+            MMIP_API.SystemCopy(new FileInfo("AppxManifest (1).xml").FullName, mcManifest);
+        }
+    }
+}
---
+++ MMIP/SDK/MMIP_API.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace MMIP.SDK
+{
+    public class MMIP_API
+    {
+        static void ExecuteCommand(string command)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine((String.IsNullOrEmpty(output) ? "" : output));
+            process.Close();
+        }
+
+        public static void TakeOwnership(string path)
+        {
+            ExecuteCommand($"takeown /f \"{path}\"");
+        }
+
+        public static void SystemCopy(string srcDir, string targetDir)
+        {
+            ExecuteCommand($"xcopy \"{srcDir}\" \"{targetDir}\\\" /o /x /e");
+        }
+
+        public static void GrantFullControl(string path)
+        {
+            try
+            {
+                // Get the current user's identity
+                var identity = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(identity);
+
+                // Grant full permissions to the file
+                var fs = new FileSecurity(path, AccessControlSections.All);
+                var accessRule = new FileSystemAccessRule(principal.Identity.Name, FileSystemRights.FullControl, AccessControlType.Allow);
+                fs.AddAccessRule(accessRule);
+                File.SetAccessControl(path, fs);
+                Console.WriteLine("Successfully granted full control to the file.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error granting full control to the file: " + ex.Message);
+            }
+        }
+    }
+}
